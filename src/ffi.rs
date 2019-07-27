80478562b7e47ef99f5bd9389233f4069e876ae4
Denis Karpovskiy <geext29@gmail.com>
2019-07-27 13:33:59 +0300

    Add the rest of algotihms to ffi

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,12 +1,13 @@
-use image::{ RgbImage, ImageError };
+use image::ImageError;
 use crate::PixelColor;
 
 use crate::{
     binary_image::BinaryImage,
-    skeletonizers::{
-        zhangsuen_skeletonizer::ZhangSuenSkeletonizer,
-        Skeletonizer
-    }
+    ZhangSuenSkeletonizer,
+    RosenfeldSkeletonizer,
+    EberlySkeletonizer,
+    Skeletonizer,
+    AdjacencyMode
 };
 
 #[repr(C)]
@@ -19,23 +20,43 @@ pub struct Buffer {
 pub extern fn improc_petrsu_zhang_suen_skeletonization(image_bytes: *const u8, len: usize) -> Buffer {
     let skeletonizer = ZhangSuenSkeletonizer::new();
 
-    let result_image = match skeletonize(image_bytes, len, skeletonizer) {
+    match skeletonize(image_bytes, len, skeletonizer) {
         Ok(img) => img,
-        Err(_) => return Buffer {
+        Err(_) => Buffer {
             data: 0 as *mut u8,
             len: 0
         }
+    }
+}
+
+#[no_mangle]
+pub extern fn improc_petrsu_rosenfeld_skeletonization(image_bytes: *const u8, len: usize, adjacency_mode: i32) -> Buffer {
+    let mode = if adjacency_mode == 0 {
+        AdjacencyMode::Eight
+    } else {
+        AdjacencyMode::Four
     };
+    let skeletonizer = RosenfeldSkeletonizer::new(mode);
 
-    let mut slice = result_image.into_raw().into_boxed_slice();
-    let data = slice.as_mut_ptr();
-    let len = slice.len();
+    match skeletonize(image_bytes, len, skeletonizer) {
+        Ok(img) => img,
+        Err(_) => Buffer {
+            data: 0 as *mut u8,
+            len: 0
+        }
+    }
+}
 
-    std::mem::forget(slice);
+#[no_mangle]
+pub extern fn improc_petrsu_eberly_skeletonization(image_bytes: *const u8, len: usize) -> Buffer {
+    let skeletonizer = EberlySkeletonizer::new();
 
-    Buffer {
-        data,
-        len
+    match skeletonize(image_bytes, len, skeletonizer) {
+        Ok(img) => img,
+        Err(_) => Buffer {
+            data: 0 as *mut u8,
+            len: 0
+        }
     }
 }
 
@@ -49,7 +70,7 @@ pub extern fn improc_petrsu_free(buf: Buffer) {
     }
 }
 
-fn skeletonize<T: Skeletonizer>(image_bytes: *const u8, len: usize, skeletonizer: T) -> Result<RgbImage, ImageError>  {
+fn skeletonize<T: Skeletonizer>(image_bytes: *const u8, len: usize, skeletonizer: T) -> Result<Buffer, ImageError>  {
     let slice = unsafe { std::slice::from_raw_parts(image_bytes, len) };
 
     let original_image = image::load_from_memory(slice)?.to_rgb();
@@ -58,5 +79,16 @@ fn skeletonize<T: Skeletonizer>(image_bytes: *const u8, len: usize, skeletonizer
 
     skeletonizer.process(&mut binary_image);
 
-    Ok(binary_image.to_rgb_image())
+    let result_image = binary_image.to_rgb_image();
+
+    let mut slice = result_image.into_raw().into_boxed_slice();
+    let data = slice.as_mut_ptr();
+    let len = slice.len();
+
+    std::mem::forget(slice);
+
+    Ok(Buffer {
+        data,
+        len
+    })
 }
