4a43fb048e53becaa9b07c588a0e27147ca43131
Denis Karpovskiy <geext29@gmail.com>
2019-07-28 10:32:06 +0300

    start to implement binary image converter

--- src/binary_image_converters/threshold_binary_image_converter.rs
+++ src/binary_image_converters/threshold_binary_image_converter.rs
@@ -1,12 +1,17 @@
-// use image::GenericImage;
-// use crate::image_processer::ImageProcesser;
+use image::RgbImage;
+use crate::image_processer::ImageProcesser;
 
-// pub struct ThresholdBinaryImageConverter {
+pub struct ThresholdBinaryImageConverter {
 
-// }
+}
 
-// impl<T: GenericImage> ImageProcesser<T> for ThresholdBinaryImageConverter {
-//     fn process(image: &T) {
-        
-//     }
-// }
+impl ImageProcesser for ThresholdBinaryImageConverter {
+    fn process(image: &RgbIMage) -> RgbImage {
+        for y in 0..image.height() {
+            for x in 0..image.width() {
+                let pixel = image.get_pixel(x, y);
+                let zero = <<<RgbImage as GenericImageView>::Pixel as Pixel>::Subpixel as Zero>::zero();
+            }
+        }
+    }
+}
